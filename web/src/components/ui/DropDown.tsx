3eee64911200178ac68da78480be6ac81438f4cc
williamstyronejr <itenory@gmail.com>
2023-08-29 21:40:01 -0400

    Updated styling

--- web/src/components/ui/DropDown.tsx
+++ web/src/components/ui/DropDown.tsx
@@ -26,7 +26,7 @@ const DropDown = ({
       <div className="w-full" ref={ref}>
         <div className={`t ${active ? "" : ""}`}>
           <button
-            className="flex align-left rounded-md p-2 bg-sky-500"
+            className="flex align-left rounded-md p-2 bg-slate-100 hover:bg-slate-300 border border-slate-500"
             type="button"
             onClick={() => {
               setActive((old) => !old);
@@ -35,9 +35,11 @@ const DropDown = ({
             <div className="flex-grow mr-2">{value !== "" ? value : title}</div>
 
             <span
-              className={`transition-transform ${active ? "rotate-180" : ""}`}
+              className={`transition-transform ${
+                active ? "rotate-90" : "-rotate-90"
+              }`}
             >
-              up
+              &gt;
             </span>
           </button>
 
@@ -45,12 +47,12 @@ const DropDown = ({
             <div
               className={`${
                 active ? "flex" : "hidden"
-              } flex-col flex-nowrap absolute w-full shadow-md z-10 origin-[top_center] bg-white`}
+              } flex-col flex-nowrap absolute w-full shadow-md z-10 origin-[top_center] bg-slate-100`}
             >
               {options.map((option) => (
                 <button
                   key={`select-${option}`}
-                  className="transition-colors"
+                  className="transition-colors my-0.5 py-0.5 hover:bg-slate-300"
                   type="button"
                   onClick={() => {
                     setActive(false);
