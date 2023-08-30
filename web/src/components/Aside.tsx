dcf371a12f9cc3e6bf3a56262c1007f90ed2d519
williamstyronejr <itenory@gmail.com>
2023-08-29 21:41:29 -0400

    Add dragging with reactdnd

--- web/src/components/Aside.tsx
+++ web/src/components/Aside.tsx
@@ -1,62 +1,98 @@
 "use client";
-import { useState } from "react";
 
-const tasks = [
-  {
-    id: "12",
-    title: "title",
-  },
-];
+import { useState } from "react";
+import { useDrag } from "react-dnd";
 
 const TaskItem = ({ id, title }: { id: string; title: string }) => {
-  const [] = useState(false);
+  const [collected, drag, dragPreview] = useDrag(() => ({
+    type: "task",
+    item: { id, title },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
 
-  return (
-    <div key={id} className="py-2">
-      {title}
+  return collected.isDragging ? (
+    <div className="bg-sky-500 w-full h-10" ref={dragPreview}></div>
+  ) : (
+    <div className="" ref={dragPreview}>
+      <div ref={drag} className="flex flex-row flex-nowrap">
+        <button
+          className="transition-colors w-5 h-5 rounded mr-2 border-2 border-black hover:border-gray-500 hover:bg-gray-500/20"
+          type="button"
+          onClick={() => {
+            console.log("TODO: Completing Event");
+          }}
+        />
+
+        <div>
+          {title}
+          <div>Gmail</div>
+        </div>
+      </div>
     </div>
   );
 };
 
-const Aside = () => {
-  const [display, setDisplay] = useState("tasks");
+const TaskList = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([
+    {
+      id: "12",
+      title: "title",
+    },
+    {
+      id: "2",
+      title: "title",
+    },
+    {
+      id: "1",
+      title: "title",
+    },
+  ]);
 
   return (
-    <aside className="flex flex-row flex-nowrap w-60">
-      <div className="w-10">
-        <button
-          className=""
-          type="button"
-          disabled={display === "tasks"}
-          onClick={() => {
-            setDisplay("tasks");
+    <>
+      <div className="py-4">
+        <input
+          className="w-full  rounded-lg px-2  py-1 bg-slate-100"
+          type="text"
+          placeholder="Add Task"
+          value={input}
+          onChange={(evt) => setInput(evt.target.value)}
+          onKeyUp={(evt) => {
+            if (evt.key === "Enter") {
+              setTasks((old) => [
+                ...old,
+                {
+                  id: Math.random().toString(),
+                  title: input,
+                },
+              ]);
+              setInput("");
+            }
           }}
-        >
-          T
-        </button>
+        />
       </div>
 
-      <div className="flex flex-col">
-        <div className="">
-          <input
-            className="w-full  rounded-lg px-2  py-1 bg-gray-500"
-            type="text"
-            placeholder="Add Task"
-            onKeyUp={(evt) => {
-              if (evt.key === "Enter") {
-                console.log("Creating Task");
-              }
-            }}
-          />
-        </div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={`task-list-${task.id}`}
+          id={task.id}
+          title={task.title}
+        />
+      ))}
+    </>
+  );
+};
 
-        {tasks.map((task) => (
-          <TaskItem
-            key={`task-list-${task.id}`}
-            id={task.id}
-            title={task.title}
-          />
-        ))}
+const Aside = () => {
+  const [display, setDisplay] = useState("calendar");
+
+  return (
+    <aside className="flex flex-row flex-nowrap w-60 pr-2">
+      <div className="flex flex-col">
+        {display === "calendar" ? <TaskList /> : null}
       </div>
     </aside>
   );
