69ca42901bef2778b3c8701cab00d3bbd1137438
williamstyronejr <itenory@gmail.com>
2023-08-29 21:42:16 -0400

    Styling and minor changes

--- web/src/components/Calendar/index.tsx
+++ web/src/components/Calendar/index.tsx
@@ -24,7 +24,7 @@ export default function Calendar({ initialEvent }: { initialEvent: any }) {
   const [interval, setInterval] = useState("Month");
   const [startDate, setStartDate] = useState(dayjs().toString());
   const currentMonth = months[dayjs(startDate).get("month")];
-  console.log(startDate);
+  const currentYear = dayjs(startDate).get("year");
 
   const moveDate = useCallback(
     (amount: number) => {
@@ -41,8 +41,10 @@ export default function Calendar({ initialEvent }: { initialEvent: any }) {
 
   return (
     <div className="flex flex-col flex-nowrap flex-grow w-full">
-      <div className="flex flex-row flex-nowrap shrink-0 justify-between">
-        <div>{currentMonth}</div>
+      <div className="flex flex-row flex-nowrap shrink-0 justify-between pt-2 pb-4">
+        <div>
+          {currentMonth} <span className="font-bold">{currentYear}</span>
+        </div>
 
         <div className="flex flex-row flex-nowrap">
           <DropDown
@@ -51,28 +53,56 @@ export default function Calendar({ initialEvent }: { initialEvent: any }) {
             changeValue={setInterval}
           />
 
-          <button className="mx-2" type="button" onClick={() => {}}>
+          <button
+            className="rounded-md mx-2 p-2 bg-slate-100 hover:bg-slate-300 border border-slate-500"
+            type="button"
+            onClick={() => {}}
+          >
             Today
           </button>
 
           <button
-            className="mx-2"
+            className="transition-colors mx-2 px-2 h-8 rounded-lg hover:bg-slate-300"
             type="button"
             onClick={() => {
               moveDate(-1);
             }}
           >
-            b
+            <svg
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 sc-vb7gpf-2 fzgCuP"
+            >
+              <path
+                d="M9.5 3.5 5 8l4.5 4.5"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
           </button>
 
           <button
-            className="mx-2"
+            className="transition-colors mx-2 px-2 h-8 rounded-lg hover:bg-slate-300"
             type="button"
             onClick={() => {
               moveDate(1);
             }}
           >
-            b
+            <svg
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+            >
+              <path
+                d="M6.5 3.5 11 8l-4.5 4.5"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
           </button>
         </div>
       </div>
